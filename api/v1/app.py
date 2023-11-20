64f2f2b07ccd707cd21455453e786af802aaf6cf
cpmoore0317 <kingpkayy@gmail.com>
2023-11-20 11:47:13 -0600

    Documentation test for app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
