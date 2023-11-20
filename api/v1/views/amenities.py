574ab36adab8e91d3abc86a2bc26b84fbb970420
cpmoore0317 <kingpkayy@gmail.com>
2023-11-20 15:27:11 -0600

    Local working for ameneties & import file to app

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""states.py"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    """Get amenity information for all amenities."""
+    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    return jsonify(amenities)
+
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity(amenity_id):
+    """Get amenity information for the specified amenity."""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Delete an amenity based on its amenity_id."""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({})
